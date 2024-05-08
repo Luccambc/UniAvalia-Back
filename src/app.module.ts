3fb22fc9cd1ae2f475cc8f73e473a19cc32560b4
Leandro Beloti Kornelius <leobelko@gmail.com>
2024-05-08 09:22:44 -0300

    🔧 fix: fix dtos & config to run server

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
 import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController, UserController],
-  providers: [AppService],
+  providers: [AppService, UserService, PrismaService],
 })
 export class AppModule {}
