0771c27debc5ad581187f52edbfbda23ece74876
Leandro Beloti Kornelius <leobelko@gmail.com>
2024-06-03 15:00:26 -0300

    ⚙️ refactor: makes each field optional with partial type

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,35 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
-import {
-  IsBase64,
-  IsEmail,
-  IsOptional,
-  IsString,
-  IsStrongPassword,
-} from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
-  @IsOptional({ message: 'Nome is required' })
-  @IsString({ message: 'Nome must be a string' })
-  nome: string;
-
-  @IsOptional({ message: 'Email is required' })
-  @IsEmail()
-  email: string;
-
-  @IsOptional({ message: 'Password is required' })
-  @IsStrongPassword()
-  password: string;
-
-  @IsOptional({ message: 'Semester is required' })
-  @IsString({ message: 'Semester must be a string' })
-  semester: string;
-
-  @IsOptional({ message: 'Major is required' })
-  @IsString({ message: 'Major must be a string' })
-  major: string;
-
-  @IsOptional({ message: 'ProfilePicture is optional' })
-  @IsBase64()
-  profilePicture: string;
-}
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
